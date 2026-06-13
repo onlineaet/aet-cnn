0cc5d5b26b5e5fe6746573ea4fbb923a3b8dcdd9
onlineaet <zclei@sina.com>
2026-06-13 11:13:48 +0800

    加入注释。

--- src/classifier_cifar.sh
+++ src/classifier_cifar.sh
@@ -1,10 +1,12 @@
-./aitest classifier train -data /home/sns/darknet-resource/cfg/classifier/cifar/cifar.data \
--cfg /home/sns/darknet-resource/cfg/classifier/cifar/cifar_small.cfg   \
+#把/home/xxx/aet-cnn-data替换为文件aet-cnn-data.tar.xz解压后所在的目录
+#-usemtcs表示用gpu训练，否则用cpu训练。
+./aitest classifier train -data /home/xxx/aet-cnn-data/cfg/classifier/cifar/cifar.data \
+-cfg /home/xxx/aet-cnn-data/cfg/classifier/cifar/cifar_small.cfg   \
 -usemtcs
 
-#./aitest classifier valid -data /home/sns/darknet-resource/cfg/classifier/cifar/cifar.data \
-#-cfg /home/sns/darknet-resource/cfg/classifier/cifar/cifar_small.cfg  -weight /home/sns/darknet-resource/backup/classfier/cifar/aet.weights \
-#-usemtcsx
+#./aitest classifier valid -data /home/xxx/aet-cnn-data/cfg/classifier/cifar/cifar.data \
+#-cfg /home/xxx/aet-cnn-data/cfg/classifier/cifar/cifar_small.cfg  -weight /home/sns/darknet-resource/backup/classfier/cifar/aet.weights \
+#-usemtcs
 
 
 
